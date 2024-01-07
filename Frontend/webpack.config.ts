e890243a52e8b979dc0aa5cdb904f493daa1e3bc
islam haluhaev <islamhaluhaev225@gmail.com>
2024-01-07 22:18:44 +0300

    firs

---
+++ Frontend/webpack.config.ts
@@ -0,0 +1,67 @@
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+type BuildMode = 'development' | 'production'
+
+interface BuildEnv {
+    mode: BuildMode,
+    port: number
+}
+
+export default (env: any) => {
+    const mode = env.mode || 'development'
+    const port = env.port || 3000
+    const isDev = mode === 'development' 
+
+    const config = {
+    mode: "development",
+    entry: path.resolve(__dirname , 'src' , 'index.tsx'),
+    output: {
+        filename: '[name][contenthash].js',
+        path: path.resolve(__dirname , 'build'),
+        clean: true
+
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public' , 'index.html')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  "style-loader",
+                  {
+                    loader: "css-loader",
+                    options: {
+                        modules: {
+                            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+                            localIdentName: isDev? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
+                        }
+                    }
+                  },
+                  "sass-loader",
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devtool: isDev ?  "inline-source-map" : undefined,
+
+    devServer:  isDev ? {
+        port: port,
+        open: true
+    } : undefined
+    };    
+
+    return config
+}
